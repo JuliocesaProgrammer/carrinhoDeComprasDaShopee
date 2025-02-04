68180439d74457f621411ad06b589e406dcec7e4
Julio Cesar Ramos de Oliveira <Juliocesarramos766@gmail.com>
2025-02-03 23:11:30 -0300

    hoje

--- Src/index.js
+++ Src/index.js
@@ -1,15 +1,22 @@
 import createItem from "./services/item.js";
 import * as cartService from "./services/cart.js";
+
 const myCart = [];	
+const myWishList = [];
 
 console.log('Hello User, seja bem vindo ao carrinho da shopee');
 
-const item1 = await createItem('Camisa', 50.99, 2);
-const item2 = await createItem('Calça', 150.99, 1);
+const item1 = await createItem("Camisa", 50.99, 2);
+const item2 = await createItem("Calça", 150.99, 3);
 
 await cartService.addItemToCart(myCart, item1);
 await cartService.addItemToCart(myCart, item2);
 
+await cartService.displayCart(myCart);
+
 console.log('Shopee Cart Total is: ');
 await cartService.calculateTotal(myCart);
+
+console.log('Shopee WishList Total is: ');
+await cartService.calculateTotal(myWishList);
 
--- Src/services/cart.js
+++ Src/services/cart.js
@@ -3,15 +3,25 @@ async function addItemToCart(userCart, item) {
 };
 
 async function calculateTotal(userCart) {
-    console.log(userCart.reduce((total, item) => total + item.subtotal(), 0));
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(result);
 }
 
 async function deleteItemFromCart(userCart, name) {
-  
+  const index = userCart.findIndex((item) => item.name === name);
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
 }
 
 async function removeItem(userCart, index) {
 
 }
 
-export { addItemToCart, calculateTotal, deleteItemFromCart, removeItem };
+async function displayCart(userCart) {
+console.log("Shopee Cart List:");
+  userCart.forEach((item, index) => {
+    console.log(`Item: ${item.name} - Quantity: ${item.quantity} - Price: ${item.price}`);
+  });
+}
+export { addItemToCart, calculateTotal, deleteItemFromCart, removeItem, displayCart};
